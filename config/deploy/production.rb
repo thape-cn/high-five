9fc88cd3325ffbb3e3775f839a3666b85b65bbca
Eric Guo <eric.guocz@gmail.com>
2024-10-21 10:11:43 +0800

    To deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
-set :application, "pp_vendor"
-set :repo_url, "https://git.thape.com.cn/rails/performance.git"
+set :application, "high_five"
+set :repo_url, "https://git.thape.com.cn/rails/high-five.git"
 set :nginx_use_ssl, true
 set :rails_env, "production"
-set :puma_service_unit_name, :puma_pp_vendor
+set :puma_service_unit_name, :puma_high_five
 set :puma_systemctl_user, :system
 
-server "thape_vendor", user: "pp_vendor", roles: %w[app db web]
+server "thape_ai", user: "high_five", roles: %w[app db web]
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :application, "high_five"
 set :repo_url, "https://git.thape.com.cn/rails/high-five.git"
 set :nginx_use_ssl, true
 set :rails_env, "production"
-set :puma_service_unit_name, :puma_rails_starter
+set :puma_service_unit_name, :puma_high_five
 set :puma_systemctl_user, :system
 
 server "bandwagon", user: "high_five", roles: %w[app db web]
