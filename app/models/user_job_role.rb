9189f12f184db1fe8729f62192cbb7d6672d7927
Eric Guo <eric.guocz@gmail.com>
2024-11-12 14:31:28 +0800

    Display position and title.

--- app/models/user_job_role.rb
+++ app/models/user_job_role.rb
@@ -2,4 +2,5 @@ class UserJobRole < ApplicationRecord
   belongs_to :user
   belongs_to :job_role
   belongs_to :manager_user, optional: true, class_name: :User
+  has_many :managed_user_job_roles, foreign_key: :manager_user_id, class_name: :UserJobRole
 end
