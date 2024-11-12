047cbce677053f95fa84c41787f9aec6adefa0e2
Eric Guo <eric.guocz@gmail.com>
2024-11-12 14:47:50 +0800

    List the managed_user_job_roles

--- app/models/user_job_role.rb
+++ app/models/user_job_role.rb
@@ -2,5 +2,5 @@ class UserJobRole < ApplicationRecord
   belongs_to :user
   belongs_to :job_role
   belongs_to :manager_user, optional: true, class_name: :User
-  has_many :managed_user_job_roles, foreign_key: :manager_user_id, class_name: :UserJobRole
+  has_many :managed_user_job_roles, class_name: "UserJobRole", foreign_key: "manager_user_id", primary_key: "user_id"
 end
