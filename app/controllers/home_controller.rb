ca273317e9c8e6b6509c55d2f01d87267b07b05c
Eric Guo <eric.guocz@gmail.com>
2024-11-14 14:00:58 +0800

    Display second level user job roles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,13 @@ class HomeController < ApplicationController
 
   def index
     @first_level_user_job_roles = current_user&.user_job_roles
+    @second_level_user_job_roles = []
+
+    @first_level_user_job_roles.each do |ujr|
+      ujr.managed_user_job_roles.each do |second_ujr|
+        @second_level_user_job_roles << second_ujr if second_ujr.managed_user_job_roles.present?
+      end
+    end
     redirect_to admin_root_path if current_user&.admin?
   end
 
