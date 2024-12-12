432b0b5d8ec3bcd3c1362843abc1f3861155835c
Eric Guo <eric.guocz@gmail.com>
2024-12-12 14:54:21 +0800

    Allow create bad events.

--- app/controllers/bad_events_controller.rb
+++ app/controllers/bad_events_controller.rb
@@ -3,4 +3,19 @@ class BadEventsController < ApplicationController
     @bad_event = BadEvent.new
     render layout: false
   end
+
+  def create
+    @bad_event = BadEvent.new(bad_event_params)
+
+    if @bad_event.valid?
+      current_user.user_bad_events.create(bad_event: @bad_event)
+      flash[:success] = t(".success")
+    end
+  end
+
+  private
+
+  def bad_event_params
+    params.require(:bad_event).permit(:bad_title)
+  end
 end
--- app/models/bad_event.rb
+++ app/models/bad_event.rb
@@ -1,4 +1,6 @@
 class BadEvent < ApplicationRecord
   has_many :user_bad_events, dependent: :destroy
   has_many :users, through: :user_bad_events
+
+  validates :bad_title, presence: true
 end
