0d85dd49892a169c2942f8114e7cbfb9c7e73910
Eric Guo <eric.guocz@gmail.com>
2024-12-23 11:14:48 +0800

    Prompt: List the user's recent GoodEvent and BadEvent in the user's show page, similar to home page, the home page list all the users GoodEvent and BadEvent, but in the user's show page, only list the user's own events. Please using the same layout similar to home page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!
 
   def show
+    @user = User.find_by!(email: "#{params[:id]}@thape.com.cn")
+    @good_events = @user.good_events
+    @bad_events = @user.bad_events
   end
 end
