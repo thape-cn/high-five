b6fed195a8c72f73ea803ac090e51a7cbf6a39e3
Eric Guo <eric.guocz@gmail.com>
2024-12-23 14:07:23 +0800

    User's show page title using the user's Chinese name.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(email: "#{params[:id]}@thape.com.cn")
+    set_meta_tags title: t(".title", name: @user.chinese_name)
     @good_events = @user.good_events
     @bad_events = @user.bad_events
   end
