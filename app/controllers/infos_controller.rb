8a42a1696de1c0588303e7f28c7418f1f5137b90
Eric Guo <eric.guocz@gmail.com>
2025-02-20 14:29:13 +0800

    Another setting.

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -6,12 +6,12 @@ class InfosController < ApplicationController
       sso_enforced_for_signin_protocol: "oidc",
       sso_enforced_for_web: false,
       sso_enforced_for_web_protocol: "oidc",
-      enable_email_code_login: false,
+      enable_email_code_login: true,
       enable_email_password_login: true,
       enable_social_oauth_login: false,
       is_allow_register: false,
       is_allow_create_workspace: false,
-      is_email_setup: false,
+      is_email_setup: true,
       license: {
         status: "active",
         expired_at: ""
