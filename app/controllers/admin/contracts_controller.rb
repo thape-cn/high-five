4f39c00bfa8b3490403c627311ef735b7b973a50
Eric-Guo <eric.guocz@gmail.com>
2025-05-23 11:08:37 +0800

    Extract the duplicate logic.

--- app/controllers/admin/contracts_controller.rb
+++ app/controllers/admin/contracts_controller.rb
@@ -9,11 +9,7 @@ module Admin
 
     def create
       file = contract_params[:file]
-      dify_chat = RubyLLM.chat(model: "dify-api", provider: :dify, assume_model_exists: true)
-      dify_chat.with_context(RubyLLM.context do |config|
-        config.dify_api_base = Rails.application.credentials.dify_base_url
-        config.dify_api_key = Rails.application.credentials.dify_api_key
-      end)
+      dify_chat = initialize_dify_chat
       response = dify_chat.upload_document(file)
       if response.status == 201
         upload_file_id = response.body[:id]
@@ -25,11 +21,7 @@ module Admin
     def invoke_ai
       field_name = params["field_name"]
       field_dify_key = Rails.application.credentials.dify_keys[field_name]
-      dify_chat = RubyLLM.chat(model: "dify-api", provider: :dify, assume_model_exists: true)
-      dify_chat.with_context(RubyLLM.context do |config|
-        config.dify_api_base = Rails.application.credentials.dify_base_url
-        config.dify_api_key = field_dify_key
-      end)
+      dify_chat = initialize_dify_chat(field_dify_key)
       response = dify_chat.ask "合同数据录入", with: @contract_basic.upload_file_id
       Rails.logger.info "invoke_ai #{field_name} with #{@contract_basic.upload_file_id}: #{response.content}"
       @contract_basic.update_attribute(field_name, response.content)
@@ -52,6 +44,15 @@ module Admin
 
     def contract_params
       params.permit(:file, :authenticity_token, :commit)
+    end
+
+    def initialize_dify_chat(api_key = Rails.application.credentials.dify_api_key)
+      dify_chat = RubyLLM.chat(model: "dify-api", provider: :dify, assume_model_exists: true)
+      dify_chat.with_context(RubyLLM.context do |config|
+        config.dify_api_base = Rails.application.credentials.dify_base_url
+        config.dify_api_key = api_key
+      end)
+      dify_chat
     end
   end
 end
