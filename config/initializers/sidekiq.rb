abd030da4a428ac5cd8ab932fcb2189458ff6eab
Eric-Guo <eric.guocz@gmail.com>
2025-06-27 15:25:48 +0800

    Ever faster.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.average_scheduled_poll_interval = 2
+  config.average_scheduled_poll_interval = 1
   if Rails.application.credentials[Rails.env.to_sym].present?
     config.redis = {url: "redis://#{Rails.application.credentials.redis_server}:#{Rails.application.credentials.redis_port}/#{Rails.application.credentials[Rails.env.to_sym][:redis_db_num]}"}
   end
