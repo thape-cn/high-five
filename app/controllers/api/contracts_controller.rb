f5719668333cadb5c705dc665ffbc9cfc5217006
Eric-Guo <eric.guocz@gmail.com>
2025-07-07 15:50:42 +0800

    Start using files_info when upload.

--- app/controllers/api/contracts_controller.rb
+++ app/controllers/api/contracts_controller.rb
@@ -7,42 +7,17 @@ module API
 
     def update
       bpm_id = params[:id]
-      file = contract_params[:file]
-      filename = contract_params[:filename]
+      files_info = contract_params[:files_info]
 
       if bpm_id.blank?
         return render json: {is_success: false, error_message: "ID 不能为空"}, status: :bad_request
       end
 
-      if file.blank?
-        return render json: {is_success: false, error_message: "文件不能为空"}, status: :bad_request
+      if files_info.blank?
+        return render json: {is_success: false, error_message: "files_info 不能为空"}, status: :bad_request
       end
+      files_info.each do |file_url|
 
-      dify_chat = initialize_dify_chat
-      response = dify_chat.provider.upload_document(file)
-
-      if response.status == 201
-        upload_file_id = response.body[:id]
-        upload_filename = filename.presence || response.body[:name]
-
-        contract_basic = ContractBasic.find_or_create_by(bpm_id: bpm_id) do |cb|
-          cb.upload_file_id = upload_file_id
-          cb.upload_filename = upload_filename
-        end
-        contract_basic.upload_file_id = upload_file_id
-        contract_basic.upload_filename = upload_filename
-        contract_basic.save
-        contract_basic.create_contract_review # always create new contract_review after file changed
-
-        render json: {
-          is_success: true,
-          bpm_id: contract_basic.bpm_id,
-          contract_id: contract_basic.id,
-          upload_file_id: upload_file_id,
-          upload_filename: upload_filename
-        }, status: :created
-      else
-        render json: {is_success: false, error_message: "文件上传失败"}, status: :unprocessable_entity
       end
     rescue => e
       Rails.logger.error "API contract creation error: #{e.message}"
@@ -76,7 +51,7 @@ module API
     end
 
     def contract_params
-      params.permit(:id, :file, :filename)
+      params.permit(:id, :files_info)
     end
   end
 end
--- app/views/api/contracts/show.json.jbuilder
+++ app/views/api/contracts/show.json.jbuilder
@@ -1,5 +1,6 @@
 json.is_success true
 json.error_message nil
+json.contract_id @contract_basic.id
 json.bpm_id @contract_basic.bpm_id
 json.upload_file_id @contract_basic.upload_file_id
 json.upload_filename @contract_basic.upload_filename
