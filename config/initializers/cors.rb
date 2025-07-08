203e1a8116834906c063a2898912e00356000128
Eric-Guo <eric.guocz@gmail.com>
2025-07-08 11:35:37 +0800

    Add port.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "staging-bpm.thape.com.cn", "bpm.thape.com.cn", "bpmmobiletest.thape.com.cn", "bpmmobile.thape.com.cn"
+    origins "staging-bpm.thape.com.cn:1000", "bpm.thape.com.cn:1000", "bpmmobiletest.thape.com.cn", "bpmmobile.thape.com.cn"
 
     resource "/api/*",
       headers: :any,
