ecc71694a5d808a57d5ca14f9e1f6b70f661b9b7
Eric-Guo <eric.guocz@gmail.com>
2025-07-11 17:25:09 +0800

    Fix bug

--- app/sidekiq/ai/contract_review_summary_job.rb
+++ app/sidekiq/ai/contract_review_summary_job.rb
@@ -2,7 +2,7 @@ class AI::ContractReviewSummaryJob
   include Sidekiq::Job
   include DifyChatInitializable
 
-  def perform(contract_review_id, field_name)
+  def perform(contract_review_id)
     contract_review = ContractReview.find(contract_review_id)
   rescue => e
     Rails.logger.error "Error in ContractReviewSummaryJob with #{contract_review.id}: #{e.message}"
